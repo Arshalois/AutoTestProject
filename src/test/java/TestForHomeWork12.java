153713fbd54f88a4ad586895e27f844629cce9d5
arshaluis <amkrtchian91@gmail.com>
2023-03-05 13:56:40 +0200

    Tests updated

--- src/test/java/TestForHomeWork12.java
+++ src/test/java/TestForHomeWork12.java
@@ -13,7 +13,6 @@ public class TestForHomeWork12 extends BaseTest {
         System.out.println("I am precondition");
         Assert.assertTrue(true);
     }
-
     @Test(dependsOnMethods = "precondition")
     public void checkLogoOnTheLoginPage() {
         HomePage homePage = new HomePage(driver);
@@ -21,7 +20,6 @@ public class TestForHomeWork12 extends BaseTest {
         LoginPage loginPage = new LoginPage(driver);
         Assert.assertTrue(loginPage.getLogo().isDisplayed());
     }
-
     @Test
     public void checkRepositoriesList() {
         HomePage homePage = new HomePage(driver);
@@ -33,20 +31,19 @@ public class TestForHomeWork12 extends BaseTest {
         ProfileForm profileForm = new ProfileForm(driver);
         profileForm.goToRepositoriesPage();
         List<String> expectedRepList = new ArrayList<>();
+        expectedRepList.add("BooksWebsite");
         expectedRepList.add("AutoTestProject");
+        expectedRepList.add("Cucumber");
         expectedRepList.add("HomeWork");
         RepositoriesPage repositoriesPage = new RepositoriesPage(driver);
         Assert.assertEquals(expectedRepList, repositoriesPage.getRepositories());
     }
-
-
     @DataProvider(name = "dataProvider2")
     public Object[][] credentialProvider() {
         return new Object[][]{
                 {"sdasdas@gmail.com", "Arsois"}, {"sdadssdas@gmail.com", "Agfis"}, {"sdasdas@gmail.comf", "Ahhlois"}
         };
     }
-
     @Test(dataProvider = "dataProvider2")
     public void verifyNegativeCredentials(String login, String password) {
         HomePage homePage = new HomePage(driver);
@@ -54,8 +51,6 @@ public class TestForHomeWork12 extends BaseTest {
         LoginPage loginPage = new LoginPage(driver);
         System.out.println("login test passed");
     }
-
-
     @Test
     public void TestIssuePage() {
         HomePage homePage = new HomePage(driver);
@@ -69,7 +64,6 @@ public class TestForHomeWork12 extends BaseTest {
         IssueEdit issueEdit = new IssueEdit(driver);
         issueEdit.editTheIssue();
     }
-
     @Test
     public void OpenGitHubArticle() {
         HomePage homePage = new HomePage(driver);
@@ -78,9 +72,7 @@ public class TestForHomeWork12 extends BaseTest {
         loginPage.successfulLogIn("Arshalois", "69111527Aa");
         GitHubLink gitHubLink = new GitHubLink(driver);
         gitHubLink.clickOnGitHubArticle();
-
     }
-
    @Test
     public void verifyNegativeLogin(){
         HomePage homePage = new HomePage(driver);
@@ -89,7 +81,6 @@ public class TestForHomeWork12 extends BaseTest {
         loginPage.loginNegative("MArshalois", "69111527Aa");
         loginPage.validateErrorMessage("Incorrect username and password.");
     }
-
    @Test
     public void verifyLogOutFromGitHub(){
         HomePage homePage = new HomePage(driver);
